455e98e044af1b2d0aacbaa546c1cc238d097963
20039200 <20039200@mydbs.ie>
2025-03-26 21:11:41 +0000

    testing 1

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -3,12 +3,11 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage, getTest } from "../controllers/message.controller.js";
 
 const router = express.Router();
-
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
 
-router.get("/test", getTest);
+router.get("/test/api", getTest);
 
 export default router;
