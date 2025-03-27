a15cf940a4b941cc41427a21ec68beff2e1300fa
20039200 <20039200@mydbs.ie>
2025-03-27 19:07:12 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
+const PORT = 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
