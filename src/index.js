5b3c4ecd4de20daf03b21507ae5e50facab818b8
20039200 <20039200@mydbs.ie>
2025-03-27 21:53:36 +0000

    fixing cookies issues all

--- src/index.js
+++ src/index.js
@@ -17,8 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://chat-app-fe-srme.onrender.com"],
-    credentials: true,
+    origin: "*",
   })
 );
 
